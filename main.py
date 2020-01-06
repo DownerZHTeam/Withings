e6d47713692b8a196698e827085d5009dbcfc4e2
DownerZHTeam <59562516+DownerZHTeam@users.noreply.github.com>
2020-01-06 22:17:28 +1100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 13:20:18 2019
+
+@author: S.Senapati
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>WELCOME TO DOWNER WITHINGS!</h1>"
