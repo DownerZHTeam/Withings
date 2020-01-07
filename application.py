617639597a00a6fdcb1f3311c4c67c7cf9d2cfaa
DownerZHTeam <59562516+DownerZHTeam@users.noreply.github.com>
2020-01-07 16:11:36 +1100

    Update application.py

--- application.py
+++ application.py
@@ -11,4 +11,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello! Withings User </h1>"
+    return "<h1><center><u>Welcome to Downer Withings Fatigue Management Trial</u></center></h1>"
